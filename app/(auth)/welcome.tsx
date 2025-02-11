a37e8e83016f571cc449db429749ce6eab28ea59
mohamedhisham404 <mohamedhisham694@gmail.com>
2025-02-11 22:33:55 +0200

    add custom button component and integrate it into the onboarding screen

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -4,10 +4,12 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { router } from "expo-router";
 import Swiper from "react-native-swiper";
 import { onboarding } from "@/constants";
+import CustomButton from "../../components/CustomButton";
 
 const welcome = () => {
     const swiperRef = useRef<Swiper>(null);
     const [activeIndex, setActiveIndex] = useState(0);
+    const isLastSlide = activeIndex === onboarding.length - 1;
 
     return (
         <SafeAreaView className="flex h-full items-center justify-between bg-white">
@@ -56,9 +58,20 @@ const welcome = () => {
                     </View>
                 ))}
             </Swiper>
+
+            <CustomButton
+                title={isLastSlide ? "Get Started" : "Next"}
+                onPress={() => {
+                    if (isLastSlide) {
+                        router.replace("/(auth)/sign-up");
+                    } else {
+                        swiperRef.current?.scrollBy(1);
+                    }
+                }}
+                className="w-11/12 mt-6"
+            />
         </SafeAreaView>
     );
 };
 
 export default welcome;
-
