1b1a26de4dd1826e669e87cd99bea0ba06056015
mohamedhisham404 <mohamedhisham694@gmail.com>
2025-02-24 20:06:20 +0200

    mplement RideCard with ride details formatting in home screen

---
+++ components/GoogleTextInput.tsx
@@ -0,0 +1,12 @@
+import { GoogleInputProps } from '@/types/type'
+import { View, Text } from 'react-native'
+
+const GoogleTextInput = ({icon, initialLocation, containerStyle, textInputBackgroundColor, handlePress}:GoogleInputProps) => {
+  return (
+    <View className={`flex flex-row items-center justify-center relative z-50 rounded-xl mb-5 ${containerStyle}`}>
+      <Text>GoogleTextInput</Text>
+    </View>
+  )
+}
+
+export default GoogleTextInput
